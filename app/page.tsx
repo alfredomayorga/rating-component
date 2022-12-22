0ff83559d380a5b010055415dab97a502d219a06
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-22 11:13:35 -0600

    refactor: components to use context

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,36 @@
+'use client'
+import SelectionContextProvider from '../context/selectionContext'
 import Content from '../components/Content'
+
 export default function Home() {
   return (
-    <div 
-      className="
+    <SelectionContextProvider>
+      <div
+        className="
         flex
         justify-center
         items-center
         h-screen
         w-full
       "
-    >
-      <div 
-        className="
+      >
+        <div
+          className="
           flex
           flex-col
           gap-6
           justify-center
           items-center
+          min-w-[412px]
           max-w-[412px]
           p-8
           bg-gradient-to-b from-[#232A34] to-[#181E27] 
-          rounded-lg
+          rounded-[38px]
         "
-      >
-        <Content />
+        >
+          <Content />
+        </div>
       </div>
-    </div>
+    </SelectionContextProvider>
   )
 }
